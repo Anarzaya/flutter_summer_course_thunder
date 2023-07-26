0c943c56afa7b9dc6a45ba5720bf9563168fb574
Thunder-6 <thunder-6@Admin-6-iMac.local>
2023-07-26 21:10:49 +0800

    flutter 11 day

---
+++ day_11_flutter/lib/basic_screen.dart
@@ -0,0 +1,33 @@
+import 'package:day_11_flutter/immutable_widget.dart';
+import 'package:flutter/material.dart';
+
+class BasicScreen extends StatelessWidget {
+  const BasicScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome to flutter'),
+        actions: [
+          Padding(padding: EdgeInsets.all(10), child: Icon(Icons.edit))
+        ],
+      ),
+      body: const Center(
+        child: AspectRatio(
+          aspectRatio: 1.0,
+          child: ImmutableWidget(),
+        ),
+      ),
+      drawer: Drawer(
+        child: Container(
+          color: Colors.lightBlue,
+          child: Center(
+            child: Text('I am a drawer'),
+          )
+        )
+      ),
+    );
+  }
+
+}
