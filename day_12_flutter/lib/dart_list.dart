74795023a3ec4a264b49491be87095acb2b06e1f
Thunder-6 <thunder-6@Admin-6-iMac.local>
2023-07-28 19:47:50 +0800

    day_12_flutter

---
+++ day_12_flutter/lib/dart_list.dart
@@ -0,0 +1,12 @@
+
+var zahiinBaraa = ['Hello', 'Tort', 'Apple', 42, true];
+
+List<int> zahiinToo = [1,3,4,5,6,7];
+var tortuud = <String> ['Pie', 'Tort', 'Pineapple Tort', 'Donut'];
+
+void main () {
+  print(zahiinBaraa);
+  print(zahiinToo);
+  print(tortuud[1]);
+  print(tortuud[3]);
+}
